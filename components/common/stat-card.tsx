99a8fb4688e4b1d9fb317e55ccd92d4ae42a9691
mehedinoorkhan <mehedinoorkhan16@gmail.com>
2026-04-09 00:19:04 +0600

    Earnings page is done without responsiveness

--- components/common/stat-card.tsx
+++ components/common/stat-card.tsx
@@ -10,19 +10,21 @@ export default function StatCard({
   text?: string
 }) {
   return (
-    <div className="w-full rounded-[16px] border border-secondary  p-4">
-      <div className="flex">
+    <div className="h-full w-full min-w-0 rounded-[16px] border border-secondary p-4">
+      <div className="flex items-start">
         {/* icon box */}
         <div className="grid min-h-10 min-w-10 place-items-center rounded-[10px] bg-brand text-primary">
           <div className="h-5 w-5">{icon}</div>
         </div>
 
         {/* title */}
-        <h3 className="ml-2.5 text-[14px] text-white">{title}</h3>
+        <h3 className="ml-2.5 min-w-0 text-sm leading-[135%] wrap-break-word text-white">
+          {title}
+        </h3>
       </div>
 
       {/* text btm */}
-      <p className="text-bold mt-3 text-xl text-white">{text}</p>
+      <p className="mt-3 text-xl font-bold text-white">{text}</p>
     </div>
   )
 }

f1c0d7c8ab032c252b2ed8ec674058665605dc34
mehedinoorkhan <mehedinoorkhan16@gmail.com>
2026-04-22 00:55:11 +0600

    fixed profileTop for Parent

--- app/(dashboards)/parent/profile-settings/page.tsx
+++ app/(dashboards)/parent/profile-settings/page.tsx
@@ -4,6 +4,9 @@ import PrivacySetting from "@/components/common/privacy-setting"
 import ProfileTop from "@/components/common/profile-top"
 
 export default function Page() {
+
+  console.log(1776798683 > Date.now())
+
   return (
     <section className="text-white">
       <ProfileTop />

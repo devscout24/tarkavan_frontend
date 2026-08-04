b56711edd95c540c06ba39e9a7e8ce00660a9ee2
dev-shakhawat <inform.shakhawat@gmail.com>
2026-08-04 11:18:43 +0600

    program date update

--- app/(public)/profile/player/[playerid]/page.tsx
+++ app/(public)/profile/player/[playerid]/page.tsx
@@ -1,66 +1,13 @@
 import ProfilePage from "./components/main-page"
 import { TPlayerProfile } from "@/types"
 import { getPlayerProfile } from "@/app/(public)/action"
-import { Metadata } from "next"
 
 type ProfilePageProps = {
   params: Promise<{
     playerid: string
   }>
 }
-
-export async function generateMetadata({
-  params,
-}: ProfilePageProps): Promise<Metadata> {
-  const { playerid } = await params
-  const res = await getPlayerProfile(String(playerid))
-
-  let data = null
-
-
-  if (
-    res &&
-    "success" in res &&
-    res.success &&
-    res.data &&
-    "data" in res.data &&
-    res.data.data
-  ) {
-    data = res.data.data 
-  }
-
-  const previewImage = data?.basic_info?.preview
-
-  return {
-    metadataBase: new URL("https://goelitesport.com") ,
-    title:  "Player Profile",
-    description: data?.basic_info?.bio || "Player profile preview",
-
-    openGraph: {
-      title:  "Player Profile",
-      description: data?.basic_info?.bio || "Player profile preview",
-      images: previewImage
-        ? [
-            {
-              url: previewImage,
-              width: 1200,
-              height: 630,
-              alt: data?.basic_info?.name || "Profile Preview",
-            },
-          ]
-        : [],
-      type: "website",
-    },
-
-    twitter: {
-      card: "summary_large_image",
-      title: data?.basic_info?.name || "Player Profile",
-      description: data?.basic_info?.bio || "Player profile preview",
-      images: previewImage ? [previewImage] : [],
-    },
-  }
-}
-
+ 
 export default async function ProfilePageFinal({ params }: ProfilePageProps) {
   const { playerid } = await params
   const res = await getPlayerProfile(String(playerid))
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,25 +6,25 @@ import { cn } from "@/lib/utils"
 import Toasting from "@/components/custom/toasting"
 import StoreProvider from "./StoreProvider"
 
-// export const metadata: Metadata = {
-//   metadataBase: new URL("https://tarkavan.thenightowl.team"),
-//   title: "GoElite",
-//   description: "Best football player app",
-//   openGraph: {
-//     title: "Go Elite",
-//     description: "Best football player app",
-//     url: "https://tarkavan.thenightowl.team/",
-//     siteName: "Go Elite",
-//     images: [
-//       {
-//         url: "https://tarkavan.thenightowl.team/uploads/preview/1779134260.png",
-//         width: 1200,
-//         height: 630,
-//       },
-//     ],
-//     type: "website",
-//   },
-// }
+export const metadata: Metadata = {
+  metadataBase: new URL("https://goelitesport.com/"),
+  title: "GoElite",
+  description: "Best football(soccer) player website",
+  openGraph: {
+    title: "Go Elite",
+    description: "Best football(soccer) player website",
+    url: "https://goelitesport.com",
+    siteName: "Go Elite",
+    images: [
+      {
+        url: "https://tarkavan.thenightowl.team/uploads/preview/1779134260.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
+}
 
 export default function RootLayout({
   children,
